964a6a496be31430afa8839ca66fd9dd80f977f0
spancavil <soancavil@gmail.com>
2024-04-05 22:07:51 -0300

    Desarrollo after 5 abril

---
+++ After 5 Abril - Hasta clase 7/App.js
@@ -0,0 +1,44 @@
+import { StyleSheet, View } from "react-native"
+import Home from "./src/screens/Home"
+import { colors } from "./src/constants/colors"
+import Header from "./src/components/Header"
+import ItemListCategory from "./src/screens/ItemListCategory"
+import { useState } from "react"
+import { useFonts } from "expo-font"
+
+const App = () => {
+  const [fontsLoaded, fontError] = useFonts({
+    'Josefin': require('./assets/JosefinSans-Regular.ttf'),
+  });
+
+  const [categorySelected, setCategorySelected] = useState("")
+  console.log(categorySelected)
+
+  if(!fontsLoaded || fontError) {
+    return null
+  }
+
+  if(fontsLoaded && !fontError) {
+    return (
+      <View style={styles.container}>
+        <Header title={"Titulo de la app"} />
+        {!categorySelected ? (
+          <Home setCategorySelected={setCategorySelected} />
+          ) : (
+          <ItemListCategory categorySelected={categorySelected} setCategorySelected ={setCategorySelected}/>
+        )}
+      </View>
+    )
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    marginTop: 30,
+    flex: 1,
+    alignItems: "center",
+    backgroundColor: colors.teal200,
+  },
+})
+
+export default App
---
+++ After 5 Abril - Hasta clase 7/src/components/Search.jsx
@@ -0,0 +1,65 @@
+import { Pressable, StyleSheet, Text, TextInput, View } from "react-native"
+import React, { useState } from "react"
+import { FontAwesome } from "@expo/vector-icons"
+import { FontAwesome5 } from "@expo/vector-icons"
+import { AntDesign } from "@expo/vector-icons"
+import { colors } from "../constants/colors"
+import { FontAwesome6 } from '@expo/vector-icons';
+
+const Search = ({ onSearch = () => {}, error = "", goBack = () => {} }) => {
+  const [keyword, setKeyword] = useState("")
+  console.log(error);
+  return (
+    <View style={styles.container}>
+      <View style={styles.inputContainer}>
+        <TextInput
+          style={styles.input}
+          placeholder="Search..."
+          value={keyword}
+          onChangeText={setKeyword}
+          />
+          {error ? <Text style={styles.errorText}>{error}</Text> : null}
+
+      </View>
+      <Pressable onPress={() => onSearch(keyword)}>
+        <FontAwesome6 name="searchengin" size={24} color="white" />
+      </Pressable>
+      <Pressable onPress={() => setKeyword("")}>
+        <FontAwesome5 name="eraser" size={24} color="white" />
+      </Pressable>
+      <Pressable onPress={goBack}>
+        <AntDesign name="back" size={24} color="white" />
+      </Pressable>
+    </View>
+  )
+}
+
+export default Search
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+    gap: 18,
+  },
+  inputContainer: {
+    flexDirection: "column",
+    justifyContent: 'center',
+    alignItems: 'start',
+    gap: 4
+  },
+  input: {
+    width: 250,
+    padding: 8,
+    fontSize: 18,
+    backgroundColor: colors.teal400,
+    color: colors.platinum,
+    borderRadius: 10,
+  },
+  errorText: {
+    color: 'tomato',
+    fontSize: 14,
+    fontFamily: 'Josefin'
+  }
+})
